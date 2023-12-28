99369ec550d35bef4c538377740d31c99be405bd
Olga Aukhadieva <sarole4ka@gmail.com>
2023-12-28 14:53:15 +0500

    Added dicts.py and test_dicts.py (task 5)

---
+++ tests/test_dicts.py
@@ -0,0 +1,22 @@
+import pytest
+
+from utils.dicts import get_val
+
+@pytest.fixture
+def collection1_fixture():
+    return {"vcs": "mercurial"}
+
+@pytest.fixture
+def collection2_fixture():
+    return {}
+
+
+def test1_get_val(collection1_fixture):
+    assert get_val(collection1_fixture, "vcs") == 'mercurial'
+    assert get_val(collection1_fixture, "vcs", "git") == 'mercurial'
+
+
+def test2_get_val(collection2_fixture):
+    assert get_val(collection2_fixture, "vcs") == 'git'
+    assert get_val(collection2_fixture, "vcs", "bazaar") == "bazaar"
+
